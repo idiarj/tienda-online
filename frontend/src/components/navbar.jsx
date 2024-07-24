b7142d9907a93308dbff6e29b11d74c1e31284d5
Ange1023 <angelinapineda2001@gmail.com>
2024-07-24 16:28:42 -0400

    estructura

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+        <Link to="/productos">Productos</Link>
+        <Link to="/vender">Vender</Link>
+        <Link to="/carrito">Carrito</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
